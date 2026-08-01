a829c3e4311569892357eecc03689467cc93faa6
Nagesh P <167490432+Nageshngu@users.noreply.github.com>
2026-08-01 18:13:46 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,18 @@
+num1=float(input("Enter the First number:"))
+num2=float(input("Enter the Second number:"))
+
+#add
+add=int(num1+num2)
+print(f"Addition:{add}")
+
+#subtract
+subtract=int(num1 - num2)
+print(f"Subtraction:{subtract}")
+
+#Mutltiply
+multiply=int(num1*num2)
+print(f"Multiplication:{multiply}")
+
+#division
+divide=num1/num2
+print(f"Division:{divide}")
